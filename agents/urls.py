d8cd84e05026d290813718b84a39a70d8760b970
mumbitasha <natasha.mutunga@strathmore.edu>
2025-11-10 11:38:48 +0300

    hhhh

--- agents/urls.py
+++ agents/urls.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
-from django.shortcuts import redirect
 from django.urls import path, include
 
 urlpatterns = [
+    # Admin panel
     path('admin/', admin.site.urls),
     
-    # Redirect root to login page
-    path('', lambda request: redirect('login')),
-    
-    # Include user authentication URLs
-    path('', include('users.urls')),
-    
-    # Include M-Pesa payments app URLs
+    # User authentication URLs (login, register, etc.)
+    path('users/', include('users.urls')),
+
+    # M-Pesa payments URLs
     path('payments/', include('payments.urls')),
 ]
